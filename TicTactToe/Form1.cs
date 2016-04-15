5ea8fb2b23f65d9267c9c8d02043adcefdb20ef2
Mike Faro <m.faro96@gmail.com>
2016-04-15 12:03:19 +0200

    Added the Check_Winner function (again).

--- TicTactToe/Form1.cs
+++ TicTactToe/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TicTactToe
@@ -17,12 +10,32 @@ namespace TicTactToe
         public Form1()
         {
             InitializeComponent();
+            turnLabel.Text = "";
+            winnerLabel.Text = "";
         }
 
         private void Button_Click(object sender, EventArgs e)
         {
             Button button = sender as Button;
-            game.Turn(button);            
+            game.Turn(button);
+            Check_Winner();
+        }
+
+        public void Check_Winner()
+        {
+            //Horizontal checks
+            if (A1.Text == "X" && A2.Text == "X" && A3.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            else if (B1.Text == "X" && B2.Text == "X" && B3.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            else if (C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
         }
     }
 }
