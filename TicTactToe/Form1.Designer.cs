0204c44ff353f9b40eef4bd49e299719b2ba0e04
Mike Faro <m.faro96@gmail.com>
2016-04-15 09:49:41 +0200

    Added labels to see whose turn it is.

--- TicTactToe/Form1.Designer.cs
+++ TicTactToe/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playingLabel = new System.Windows.Forms.Label();
+            this.turnLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playingLabel
+            // 
+            this.playingLabel.AutoSize = true;
+            this.playingLabel.Location = new System.Drawing.Point(13, 236);
+            this.playingLabel.Name = "playingLabel";
+            this.playingLabel.Size = new System.Drawing.Size(47, 13);
+            this.playingLabel.TabIndex = 0;
+            this.playingLabel.Text = "Playing: ";
+            // 
+            // turnLabel
+            // 
+            this.turnLabel.AutoSize = true;
+            this.turnLabel.Location = new System.Drawing.Point(67, 236);
+            this.turnLabel.Name = "turnLabel";
+            this.turnLabel.Size = new System.Drawing.Size(0, 13);
+            this.turnLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.turnLabel);
+            this.Controls.Add(this.playingLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label playingLabel;
+        private System.Windows.Forms.Label turnLabel;
     }
 }
 
