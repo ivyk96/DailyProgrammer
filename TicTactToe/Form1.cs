59963d0c584a70faa78e827b540f08d1be141cf8
Mike Faro <m.faro96@gmail.com>
2016-04-15 13:13:17 +0200

    Added vertical and diagonal checks.

--- TicTactToe/Form1.cs
+++ TicTactToe/Form1.cs
@@ -23,6 +23,7 @@ namespace TicTactToe
         public void Check_Winner()
         {
             //Horizontal checks
+            // X
             if (A1.Text == "X" && A2.Text == "X" && A3.Text == "X")
             {
                 winnerLabel.Text = "X wins!";
@@ -34,6 +35,67 @@ namespace TicTactToe
             else if (C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
             {
                 winnerLabel.Text = "X wins!";
+            }
+            // O
+            else if (A1.Text == "O" && A2.Text == "O" && A3.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+            else if (B1.Text == "O" && B2.Text == "O" && B3.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+            else if (C1.Text == "O" && C2.Text == "O" && C3.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+
+            //Vertical checks
+            // X
+            if (A1.Text == "X" && B1.Text == "X" && C1.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            else if (A2.Text == "X" && B2.Text == "X" && C2.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            else if (A3.Text == "X" && B3.Text == "X" && C3.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            // O
+            else if (A1.Text == "O" && B1.Text == "O" && C1.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+            else if (A2.Text == "O" && B2.Text == "O" && C2.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+            else if (A3.Text == "O" && B3.Text == "O" && C3.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+
+            //Diagonal checks
+            // X
+            if (A1.Text == "X" && B2.Text == "X" && C3.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            else if (A3.Text == "X" && B2.Text == "X" && C1.Text == "X")
+            {
+                winnerLabel.Text = "X wins!";
+            }
+            // O
+            else if (A1.Text == "O" && B2.Text == "O" && C3.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
+            }
+            else if (A3.Text == "O" && B2.Text == "O" && C1.Text == "O")
+            {
+                winnerLabel.Text = "O wins!";
             }
         }
     }
