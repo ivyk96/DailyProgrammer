d4104dc8aee306a6f3b1c688ee79e1754ad56cc1
Xander Bras <99023172@mydavinci.nl>
2016-04-19 14:46:10 +0200

    Added AIFactory with a randomAI.

---
+++ TicTactToe/AIFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTactToe
+{
+    public class AIFactory
+    {
+        private Random random = new Random();
+        private GameFactory game;
+
+        public AIFactory(GameFactory game)
+        {
+            this.game = game;       
+        }
+
+        public Button Turn(IEnumerable<Button> buttonList)
+        {
+            List<Button> list = buttonList.ToList();
+            list.RemoveAt(0);
+            List<Button> tempList = new List<Button>();
+
+            foreach(Button b in list)
+            {
+                if (b.Enabled == true)
+                    tempList.Add(b);
+            }
+
+            if (tempList.Count != 0)
+                return tempList[random.Next(0, tempList.Count)];
+            else
+                return null;
+        }
+    }
+}
--- TicTactToe/Form1.cs
+++ TicTactToe/Form1.cs
@@ -8,12 +8,27 @@ namespace TicTactToe
     public partial class Form1 : Form
     {
         private GameFactory game = new GameFactory();
+        private AIFactory ai;
 
         public Form1()
         {
             InitializeComponent();
+            ai = new AIFactory(game);
             turnLabel.Text = game.GetTurn();
             winnerLabel.Text = "";
+        }
+
+        private void AI_Click()
+        {
+            Button b = ai.Turn(Controls.OfType<Button>());
+
+            if (b != null)
+            {
+                turnLabel.Text = game.PlayTurn(b);
+                winnerLabel.Text = game.CheckWinner(this.Controls.OfType<Button>());
+                winXLabel.Text = game.XWins.ToString();
+                winOLabel.Text = game.OWins.ToString();
+            }
         }
 
         private void Button_Click(object sender, EventArgs e)
@@ -22,6 +37,7 @@ namespace TicTactToe
             winnerLabel.Text = game.CheckWinner(this.Controls.OfType<Button>());
             winXLabel.Text = game.XWins.ToString();
             winOLabel.Text = game.OWins.ToString();
+            AI_Click();
         }
 
         private void restartButton_Click(object sender, EventArgs e)
