1213642695e4c47d49b1f80b6fbccbeee562911b
Xander Bras <99023172@mydavinci.nl>
2016-04-19 09:47:13 +0200

    Put all game logic in the GameFactory.

--- TicTactToe/Form1.cs
+++ TicTactToe/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace TicTactToe
@@ -17,86 +19,7 @@ namespace TicTactToe
         private void Button_Click(object sender, EventArgs e)
         {
             turnLabel.Text = game.PlayTurn((Button)sender);
-            Check_Winner();
-        }
-
-        public void Check_Winner()
-        {
-            //Horizontal checks
-            // X
-            if (A1.Text == "X" && A2.Text == "X" && A3.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            else if (B1.Text == "X" && B2.Text == "X" && B3.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            else if (C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            // O
-            else if (A1.Text == "O" && A2.Text == "O" && A3.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-            else if (B1.Text == "O" && B2.Text == "O" && B3.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-            else if (C1.Text == "O" && C2.Text == "O" && C3.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-
-            //Vertical checks
-            // X
-            if (A1.Text == "X" && B1.Text == "X" && C1.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            else if (A2.Text == "X" && B2.Text == "X" && C2.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            else if (A3.Text == "X" && B3.Text == "X" && C3.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            // O
-            else if (A1.Text == "O" && B1.Text == "O" && C1.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-            else if (A2.Text == "O" && B2.Text == "O" && C2.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-            else if (A3.Text == "O" && B3.Text == "O" && C3.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-
-            //Diagonal checks
-            // X
-            if (A1.Text == "X" && B2.Text == "X" && C3.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            else if (A3.Text == "X" && B2.Text == "X" && C1.Text == "X")
-            {
-                winnerLabel.Text = "X wins!";
-            }
-            // O
-            else if (A1.Text == "O" && B2.Text == "O" && C3.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
-            else if (A3.Text == "O" && B2.Text == "O" && C1.Text == "O")
-            {
-                winnerLabel.Text = "O wins!";
-            }
+            winnerLabel.Text = game.CheckWinner(this.Controls.OfType<Button>());
         }
     }
 }
--- TicTactToe/GameFactory.cs
+++ TicTactToe/GameFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace TicTactToe
 {
@@ -29,6 +31,90 @@ namespace TicTactToe
 
             b.Enabled = false;
             return GetTurn();
+        }
+
+        public string CheckWinner(IEnumerable<Button> buttonList)
+        {
+            List<Button> list = buttonList.ToList();
+            string winner = "";
+
+            //Horizontal checks
+            // X
+            if (list[8].Text == "X" && list[7].Text == "X" && list[6].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            else if (list[5].Text == "X" && list[4].Text == "X" && list[3].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            else if (list[2].Text == "X" && list[1].Text == "X" && list[0].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            // O
+            else if (list[8].Text == "O" && list[7].Text == "O" && list[6].Text == "O")
+            {
+                winner = "O wins!";
+            }
+            else if (list[5].Text == "O" && list[4].Text == "O" && list[3].Text == "O")
+            {
+                winner = "O wins!";
+            }
+            else if (list[2].Text == "O" && list[1].Text == "O" && list[0].Text == "O")
+            {
+                winner = "O wins!";
+            }
+
+            //Vertical checks
+            // X
+            if (list[8].Text == "X" && list[5].Text == "X" && list[2].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            else if (list[7].Text == "X" && list[4].Text == "X" && list[1].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            else if (list[6].Text == "X" && list[3].Text == "X" && list[0].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            // O
+            else if (list[8].Text == "O" && list[5].Text == "O" && list[2].Text == "O")
+            {
+                winner = "O wins!";
+            }
+            else if (list[7].Text == "O" && list[4].Text == "O" && list[1].Text == "O")
+            {
+                winner = "O wins!";
+            }
+            else if (list[6].Text == "O" && list[3].Text == "O" && list[0].Text == "O")
+            {
+                winner = "O wins!";
+            }
+
+            //Diagonal checks
+            // X
+            if (list[8].Text == "X" && list[4].Text == "X" && list[0].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            else if (list[6].Text == "X" && list[4].Text == "X" && list[2].Text == "X")
+            {
+                winner = "X wins!";
+            }
+            // O
+            else if (list[8].Text == "O" && list[4].Text == "O" && list[0].Text == "O")
+            {
+                winner = "O wins!";
+            }
+            else if (list[6].Text == "O" && list[4].Text == "O" && list[2].Text == "O")
+            {
+                winner = "O wins!";
+            }
+
+            return winner;
         }
     }
 }
