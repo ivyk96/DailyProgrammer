2ec51f7444b84e79b14ebfe8c1a70872400eed9f
Mike Faro <m.faro96@gmail.com>
2016-04-19 12:08:51 +0200

    Added a winner counter and removed a lot of unnecessary brackets.

--- TicTactToe/GameFactory.cs
+++ TicTactToe/GameFactory.cs
@@ -7,6 +7,8 @@ namespace TicTactToe
     public class GameFactory
     {
         private int turn = 0;
+        private int xWins = 0;
+        private int oWins = 0;
 
         public string GetTurn()
         {
@@ -57,78 +59,51 @@ namespace TicTactToe
             //Horizontal checks
             // X
             if (list[0].Text == "X" && list[1].Text == "X" && list[2].Text == "X")
-            {
                 winner = "X wins!";
-            }
             else if (list[3].Text == "X" && list[4].Text == "X" && list[5].Text == "X")
-            {
                 winner = "X wins!";
-            }
             else if (list[6].Text == "X" && list[7].Text == "X" && list[8].Text == "X")
-            {
                 winner = "X wins!";
-            }
             // O
             else if (list[0].Text == "O" && list[1].Text == "O" && list[2].Text == "O")
-            {
                 winner = "O wins!";
-            }
             else if (list[3].Text == "O" && list[4].Text == "O" && list[5].Text == "O")
-            {
                 winner = "O wins!";
-            }
             else if (list[6].Text == "O" && list[7].Text == "O" && list[8].Text == "O")
-            {
                 winner = "O wins!";
-            }
 
             //Vertical checks
             // X
             if (list[0].Text == "X" && list[3].Text == "X" && list[6].Text == "X")
-            {
                 winner = "X wins!";
-            }
             else if (list[1].Text == "X" && list[4].Text == "X" && list[7].Text == "X")
-            {
                 winner = "X wins!";
-            }
             else if (list[2].Text == "X" && list[5].Text == "X" && list[8].Text == "X")
-            {
                 winner = "X wins!";
-            }
             // O
             else if (list[0].Text == "O" && list[3].Text == "O" && list[6].Text == "O")
-            {
                 winner = "O wins!";
-            }
             else if (list[1].Text == "O" && list[4].Text == "O" && list[7].Text == "O")
-            {
                 winner = "O wins!";
-            }
             else if (list[2].Text == "O" && list[5].Text == "O" && list[8].Text == "O")
-            {
                 winner = "O wins!";
-            }
 
             //Diagonal checks
             // X
             if (list[0].Text == "X" && list[4].Text == "X" && list[8].Text == "X")
-            {
                 winner = "X wins!";
-            }
             else if (list[2].Text == "X" && list[4].Text == "X" && list[6].Text == "X")
-            {
                 winner = "X wins!";
-            }
             // O
             else if (list[0].Text == "O" && list[4].Text == "O" && list[8].Text == "O")
-            {
                 winner = "O wins!";
-            }
             else if (list[2].Text == "O" && list[4].Text == "O" && list[6].Text == "O")
-            {
                 winner = "O wins!";
-            }
+
+            if (winner == "X wins!")
+                xWins++;
+            else
+                oWins++;
 
             if (winner != string.Empty)
                 DisableButtons(list);
