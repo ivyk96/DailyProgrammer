77d147bb1bbc7cfa9aa5d9422237c67532d74fc9
Xander Bras <99023172@mydavinci.nl>
2016-04-21 09:57:00 +0200

    More prototype monopoly stuff.

---
+++ Monopoly/Classes/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Classes
+{
+    public class Player
+    {
+        public string Name;
+        public int Cash;
+        public int Turn;
+        public int Position;
+
+        public Player(string name)
+        {
+            Name = name;
+            Cash = 1500;
+            Position = 1;
+        }
+    }
+}
